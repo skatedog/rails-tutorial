a3cdc5d57a6f0f1f39e89fea5b296de51d3f21a3
skatedog <skatedog0001@gmail.com>
2021-09-27 01:56:26 +0000

    Add user following

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -15,6 +15,8 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_select 'h1>img.gravatar'
     assert_match @user.microposts.count.to_s, response.body
     assert_select 'div.pagination', count: 1
+    assert_select "strong#following", @user.following.count.to_s
+    assert_select "strong#followers", @user.followers.count.to_s
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
